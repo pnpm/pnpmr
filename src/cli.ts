e41059764aad1d4a624470d825708ab11969e59e
zkochan <zoltan.kochan@gmail.com>
2017-04-10 10:31:38 +0300

    feat: run commands on dependencies first
    ...then on dependents

--- src/cli.ts
+++ src/cli.ts
@@ -2,6 +2,8 @@
 import path = require('path')
 import spawn = require('cross-spawn-promise')
 import findPackages from 'find-packages'
+import createPkgGraph, {PackageNode} from 'pkgs-graph'
+import sortPkgs = require('sort-pkgs')
 
 const args = process.argv.slice(2)
 
@@ -9,13 +11,16 @@ main()
 
 async function main() {
   const pkgs = await findPackages(process.cwd())
+  const graph = createPkgGraph(pkgs)
+  const sortedPkgNodes = sortPkgs(graph)
 
   try {
-    for (let pkg of pkgs) {
+    for (let pkg of sortedPkgNodes) {
       console.log(`Running at ${pkg.path}`)
       await spawn('pnpm', args, {stdio: 'inherit', cwd: pkg.path})
     }
   } catch (err) {
+    console.log(err)
     process.exit(1)
   }
 }
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -2,3 +2,8 @@ declare module 'cross-spawn-promise' {
   const anything: any;
   export = anything;
 }
+
+declare module 'sort-pkgs' {
+  const anything: any;
+  export = anything;
+}
